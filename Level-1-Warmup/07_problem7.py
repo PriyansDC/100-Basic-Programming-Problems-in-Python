bd7c6fa8772ebba8f0cb86053194f4da67f5e38f
PriyansDC <priyanshu9803@gmail.com>
2026-07-24 14:35:03 +0530

    Revisited and solved

--- Level-1-Warmup/07_problem7.py
+++ Level-1-Warmup/07_problem7.py
@@ -1,1 +1,11 @@
 # Write a program to swap two numbers without using a third variable.
+a = 10
+b = 5
+
+# Swapping the values of a and b without using a third variable
+a = a + b 
+b = a - b
+a = a - b
+
+# Printing the values
+print(f"a: {a} \nb: {b}")
