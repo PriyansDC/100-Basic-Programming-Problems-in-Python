8f220c9d84ce3beacc8218f964b2b995d87eed51
PriyansDC <priyanshu9803@gmail.com>
2026-07-27 12:36:23 +0530

    Sovled more questions

--- Level-3-Loops on N/27_problem27.py
+++ Level-3-Loops on N/27_problem27.py
@@ -1,1 +1,10 @@
 # Write a program to find the sum of all odd numbers from 1 to n.
+
+n = int(input("Enter a number: "))
+
+result = 0
+
+for i in range(1, n+1, 2):
+    result += i
+
+print(f"Sum of all odd numbers from 1 to {n}: {result}")
