14324e16b45d585a376b50bdb1f739d7a5f010c9
PriyansDC <priyanshu9803@gmail.com>
2026-07-28 14:03:37 +0530

    Solved more questions

---
+++ Level-4-Working with the digits of a numbe/34_problem34.py
@@ -0,0 +1,18 @@
+# Write a program to display all the digits of a number n (one per line).
+
+# n = int(input("Enter a number: "))
+# rev = 0
+
+# while n > 0:
+#     digit = n % 10
+#     rev = rev * 10 + digit
+#     n //= 10
+
+# while rev > 0:
+#     print(rev % 10)
+#     rev //= 10456
+
+n = input("Enter a number: ")
+
+for i in range(len(n)):
+    print(n[i])
