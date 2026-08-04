63459e7167cdd463b53a216b942f671e98bf85c3
PriyansDC <priyanshu9803@gmail.com>
2026-08-04 21:18:04 +0530

    Solved more question

--- Level-5-Number classification/47_problem4.py
+++ Level-5-Number classification/47_problem4.py
@@ -1,1 +1,23 @@
 # Write a program to check whether a number is an Armstrong number.
+
+n = int(input("Enter a number: "))
+
+original = n
+temp = n
+num = 0
+count = 0
+
+while temp > 0:
+    count += 1
+    temp //= 10
+
+while n > 0:
+    digit = n % 10
+    num += digit ** count
+
+    n //= 10
+    
+if original == num:
+    print("Yes, it's an armstrong number")
+else:
+    print("Oops! It's not an armstrong number")
