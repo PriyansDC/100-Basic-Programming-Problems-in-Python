c840ddb15242a93bb8ccb87a26971d2a0ad4d372
PriyansDC <priyanshu9803@gmail.com>
2026-08-11 08:37:01 +0530

    Solved the question using the flow.

--- Level-5-Number classification/51_problem8.py
+++ Level-5-Number classification/51_problem8.py
@@ -13,3 +13,18 @@
 # Compare those digits with the original number
 #         ↓
 # If they match → Automorphic
+
+n = int(input("Enter a number: "))
+
+count = len(str(n))
+intCount = int(count)
+
+sq = n ** 2
+
+last_digits = sq % (10 ** count)
+
+if last_digits == n:
+    print("Yes, it's an automorphic number")
+else:
+    print("No, it's not an automorphic number")
+
