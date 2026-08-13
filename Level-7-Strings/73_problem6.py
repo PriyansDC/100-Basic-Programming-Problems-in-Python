978e5f5036dd74e21a8442687107e6c23b03fa88
PriyansDC <priyanshu9803@gmail.com>
2026-08-13 13:08:28 +0530

    Solved using gpt

--- Level-7-Strings/73_problem6.py
+++ Level-7-Strings/73_problem6.py
@@ -1,2 +1,25 @@
 # Write a program to convert a string to uppercase and lowercase without inbuilt case functions.
 
+sentence = input("Enter a string: ")
+
+uppercase = ""
+lowercase = ""
+
+for char in sentence:
+
+    ascii_value = ord(char)
+
+    if 'a' <= char <= 'z':
+        uppercase += chr(ascii_value - 32)
+        lowercase += char
+
+    elif 'A' <= char <= 'Z':
+        uppercase += char
+        lowercase += chr(ascii_value + 32)
+
+    else:
+        uppercase += char
+        lowercase += char
+
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
